31432df20c41bd30faa2f3f3078e80fd9699e488
MounikaMunnaluri <92863998+MounikaMunnaluri@users.noreply.github.com>
2023-06-07 19:23:07 +0530

    created js file for usercard2

---
+++ userCard2.js
@@ -0,0 +1,25 @@
+let gandhioldinfo = {
+    imgurl: "http://upload.wikimedia.org/wikipedia/commons/7/70/Gandhi_smiling_R.jpg",
+    name: "Mahatma Gandhi",
+    Description: "Freedom Fighter"
+}
+
+let gandhiyounginfo = {
+    imgurl: "https://i0.wp.com/www.zestvine.com/wp-content/uploads/2018/03/mahatma-gandhi-young-the-man-who-change-the-world.jpg?resize=620%2C680",
+    name: "Mohandas Karamchand Gandhi Gandhi",
+    Description: "Father of the Nation"
+}
+
+let isgandhiold = true;
+let displayobject;
+let flipdate = function(){
+    if(isgandhiold == true){
+        displayobject = gandhiyounginfo;
+        isgandhiold = false;
+    }
+    else{
+        displayobject = gandhioldinfo;
+        isgandhiold = true;
+    }
+    document.getelementById("gandhi-img")
+}
